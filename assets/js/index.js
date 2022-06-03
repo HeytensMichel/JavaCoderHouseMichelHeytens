a7a90ab08b815a878361803b8b60d75f95eb73fd
HEYTENS Michel <sealoperatorone@gmail.com>
2022-06-03 11:37:49 -0300

    Funciones basicas

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,38 @@
-function name () {
-    let nameEntry = prompt('Ingrese su nombre')
-    console.log ((nameEntry.toLowerCase()))
+let nameEntry = prompt('Ingrese su nombre');
+let ageEntry = parseInt(prompt('Ingrese su edad'));
+let appointmentDayMinusForty
+let appointmentDayPlusForty
+
+
+
+
+function name() {
+    console.log(('Nombre del paciente: ' + nameEntry.toLowerCase()))
 }
 
-function age () {
-    let ageEntry = parseInt(prompt('Ingrese su edad'));
-    if (ageEntry <= 17) { 
-        alert('Usted debe acudir a un Neurologo/a pediatrico')
-    } else (console.log(ageEntry))
+function age() {
+    if (ageEntry <= 17) {
+        alert('Usted debe acudir a un especialista pediatrico')
+    } else (console.log('Edad del paciente: ' + ageEntry))
+}
+
+function appointmentTime() {
+    if (ageEntry >= 18 && ageEntry <= 40) {
+        console.log('Horario de atencion: 08:00am-12:00am')
+    } else (console.log('Horario de atencion: 13:00pm-18-00pm'))
+}
+
+function appointmentDay() {
+    if (ageEntry <= 40 && ageEntry >= 18) {
+        let selection = (prompt('¿Asistira el dia: Lunes, Martes o Sabado?'))
+        console.log('Asistira el dia: ' + (selection.toLowerCase()))
+    } else {
+        let selection = prompt('¿Asistira el dia: Miercoles, Jueves o Viernes?')
+        console.log('Asistira el dia: ' + (selection.toLowerCase()))
+}
 }
 
 name()
 age()
+appointmentTime()
+appointmentDay()
