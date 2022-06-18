67254afaf4e3372fe83962f782b031de6599ee7e
HEYTENS Michel <sealoperatorone@gmail.com>
2022-06-18 16:13:02 -0300

    Re-armado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,89 +1,22 @@
-const patientList = ["Marcos Zabala", "Nicolas Hernandez", "Beatriz Girafales"]
-let turns = 10
-let nameEntry = prompt('Ingrese su nombre y apellido'); {
-    do {
-        patientList.push(nameEntry.toLowerCase())
-    } while (patientList.lenght <= turns)
-}
-
-let ageEntry = parseInt(prompt('Ingrese su edad'));
-let motiveEntry = parseInt(prompt(`Indique con el numero correspondiente si tuvo alguno de los siguientes: 
-1-Dolor de cabeza 
-2-Movimientos anormales en extremidades
-3-Traumatismo en la cabeza
-4-Perdida del conocimiento
-5-Pérdida de fuerzas de una mitad del cuerpo
-0-Ninguno`));
-let selection;
-let simptoms = simpDesc();
-let hospitalSelection = parseInt(prompt('Seleccione su lugar de consulta: 1- Hospital Español 2- Hospital de clinicas.'))
-
-function name() {
-    console.log('Nombre del paciente: ' + nameEntry.toLowerCase())
-}
-
-function age() {
-    if (ageEntry <= 17) {
-        alert('Usted debe acudir a un especialista pediatrico')
-    } else (console.log('Edad del paciente: ' + ageEntry))
-}
-
-function appointmentTime() {
-    if (ageEntry >= 18 && ageEntry <= 40) {
-        console.log('Horario de atencion: 08:00am-12:00am')
-    } else (console.log('Horario de atencion: 13:00pm-18-00pm'))
-}
-
-function appointmentDay() {
-    if (ageEntry <= 40 && ageEntry >= 18) {
-        let selection = (prompt('¿Asistira el dia: Lunes, Martes o Sabado?'))
-        console.log('Asistira el dia: ' + (selection.toLowerCase()))
-    } else {
-        let selection = prompt('¿Asistira el dia: Miercoles, Jueves o Viernes?')
-        console.log('Asistira el dia: ' + (selection.toLowerCase()))
-    } console.log("Pacientes registrados hasta el momento: "+ patientList)
-}
-
-function triage() {
-    switch (motiveEntry) {
-        case 1:
-            motiveEntry = "1";
-            console.log("El paciente ha indicado dolores de cabeza")
-            console.log("Descripcion de sintomatologia: " + simptoms)
-            break;
-        case 2:
-            motiveEntry = "2";
-            console.log("El paciente ha indicado movimientos anormales en extremidades")
-            console.log("Descripcion de sintomatologia: " + simptoms)
-            break;
-        case 3:
-            motiveEntry = "3";
-            console.log("El paciente ha indicado traumatismo en la cabeza")
-            console.log("Descripcion de sintomatologia: " + simptoms)
-            alert('Es recomedable que asista a un centro de salud previo a su visita.')
-            break;
-        case 4:
-            motiveEntry = "4";
-            console.log("El paciente ha indicado perdida del conocimiento")
-            console.log("Descripcion de sintomatologia: " + simptoms)
-            alert('Se exhorta a asistir a un centro de salud a la brevedad previo a su consulta')
-            break;
-        case 5:
-            motiveEntry = "5";
-            console.log("El paciente ha indicado perdida de fuerza en un hemisferio")
-            console.log("Descripcion de sintomatologia: " + simptoms)
-            alert('Asistir inmediatamente a un centro de salud. La Dra. Beltrame sera informada de su situacion.')
-            break;
-        case 6:
-            motiveEntry = "0";
-            console.log("El paciente no ha indicado sintomas.")
-            alert('Su cita ha sido confirmada con exito')
-            break;
+class Patient {
+    constructor(nombre, edad, patologias, medicacion, motivo, triage) {
+        this.name = nombre;
+        this.age = edad;
+        this.pats = patologias;
+        this.meds = medicacion;
+        this.motive = motivo;
     }
 }
-
-function simpDesc() {
-    return prompt('De ser necesario, escriba brevemente su sintomatologia');
+//
+const patient1 = new Patient("Marcos Zabala", "22", "Asma severa", "Ventolin, Prednisona", "Falta de aire",)
+const patient2 = new Patient("Martina Zaldivar", "55", "Osteoporosis", "Tramadol", "Poca movilidad en piernas",)
+const patient3 = new Patient(prompt("NOMBRE DEL PACIENTE"), prompt("EDAD DEL PACIENTE"), prompt("PATOLOGIAS PREVIAS"), prompt("MEDICACION RECETADA"), prompt("MOTIVO DE CONSULTA"),)
+const patientList = [patient1, patient2,]
+let turns = 10
+let patEntry = patient3; {
+    do {
+        patientList.push(patient3)
+    } while (patientList.length >= turns)
 }
 
 class Doctor {
@@ -92,26 +25,76 @@ class Doctor {
         this.especialty = especialidad;
         this.place = consultorio;
     }
-    hablar() {
-        console.log("Hola soy "+ this.name)
-}
 }
 const doctor1 = new Doctor("Beltrame Valentina", "Neurologia", "Hospital de Clinicas")
-const doctor2 = new Doctor("Beltrame Pablo", "Neurologia", "Hospital Español")
+const doctor2 = new Doctor("Michel Heytens", "Emergencia", "Hospital Español")
 
-function hospSelect() {
-    if (hospitalSelection === 1) {
-        console.log('Usted sera atendido en: ' + doctor2.place + ' por: ' + doctor2.name + " TURNO: " + (patientList.length))
-    } else if (hospitalSelection === 2) {
-        console.log('Usted sera atendido en: ' + doctor1.place + ' por: ' + doctor1.name + " TURNO: " + (patientList.length))
-    } else { alert('Ingrese un dato valido') }
+let motiveEntry = parseInt(prompt(`Indique el nivel de triage del paciente: 
+1-Sintomatologia sin urgencia.
+2-Sintomatologia leve sin riesgo de vida.
+3-Sintomatologia moderada con dolores o impedimentos.
+4-Sintomatologia con dolores o impedimentos considerables.
+5-Sintomatologia con riesgo de vida, dolores fuertes, impedimentos severos.
+0-Ninguna sintomatologia`));
+
+function triage() {
+    switch (motiveEntry) {
+        case 1:
+            motiveEntry = "1";
+            console.log("El paciente " + patient3.name + " sera atendido en: " + doctor2.place + " por " + doctor2.name)
+            console.log("Descripcion de sintomatologia: " + patient3.motive)
+            let day = (prompt('¿Asistira el dia: Lunes, Martes o Sabado?'))
+            console.log('Asistira el dia: ' + (day.toLowerCase() + " con el numero: " + (patientList.length)))
+            if (patient3.age >= 18 && patient3.age <= 40) {
+                console.log('Horario de atencion: 08:00am-12:00am')
+            } else (console.log('Horario de atencion: 13:00pm-18-00pm'))
+            break;
+        case 2:
+            motiveEntry = "2";
+            console.log("El paciente " + patient3.name + " sera atendido en: " + doctor2.place + " por " + doctor2.name)
+            console.log("Descripcion de sintomatologia: " + patient3.motive)
+            let dayTwo = (prompt('¿Asistira el dia: Lunes, Martes o Sabado?'))
+            console.log('Asistira el dia: ' + (dayTwo.toLowerCase() + " con el numero: " + (patientList.length)))
+            if (patient3.age >= 18 && patient3.age <= 40) {
+                console.log('Horario de atencion: 08:00am-12:00am')
+            } else (console.log('Horario de atencion: 13:00pm-18-00pm'))
+            break;
+        case 3:
+            motiveEntry = "3";
+            console.log("El paciente " + patient3.name + " sera atendido en: " + doctor2.place + " por " + doctor2.name)
+            console.log("Descripcion de sintomatologia: " + patient3.motive)
+            let dayThree = (prompt('¿Asistira el dia: Lunes, Martes o Sabado?'))
+            console.log('Asistira el dia: ' + (dayThree.toLowerCase() + " con el numero: " + (patientList.length)))
+            if (patient3.age >= 18 && patient3.age <= 40) {
+                console.log('Horario de atencion: 08:00am-12:00am')
+            } else (console.log('Horario de atencion: 13:00pm-18-00pm'))
+            break;
+            break;
+        case 4:
+            motiveEntry = "4";
+            console.log("El paciente " + patient3.name + " sera atendido en: " + doctor1.place + " por " + doctor1.name)
+            console.log("Descripcion de sintomatologia: " + patient3.motive)
+            console.log("El paciente " + patient3.name + " debe ser trasladado a emergencia y ser atendido con el numero: " + (patientList.length))
+            break;
+        case 5:
+            motiveEntry = "5";
+            console.log("El paciente " + patient3.name + " sera atendido en: " + doctor1.place + " por " + doctor1.name)
+            console.log("Descripcion de sintomatologia: " + patient3.motive)
+            console.log("El paciente " + patient3.name + " debe ser trasladado a emergencia para ser atendido de manera urgente, se ha comunicado a la emergencista " + doctor1.name)
+            break;
+        case 6:
+            motiveEntry = "0";
+            console.log("El paciente " + patient3.name + " sera atendido en: " + doctor2.place + " por " + doctor2.name)
+            console.log("Descripcion de sintomatologia: " + patient3.motive)
+            let dayFour = (prompt('¿Asistira el dia: Lunes, Martes o Sabado?'))
+            console.log('Asistira el dia: ' + (dayFour.toLowerCase() + " con el numero: " + (patientList.length)))
+            if (patient3.age >= 18 && patient3.age <= 40) {
+                console.log('Horario de atencion: 08:00am-12:00am')
+            } else (console.log('Horario de atencion: 13:00pm-18-00pm'))
+            break;
+    }
 }
 
-name()
-age()
-hospSelect()
-appointmentDay()
-appointmentTime()
-triage()
+triage ()
 
-
+console.log(patientList.length)
